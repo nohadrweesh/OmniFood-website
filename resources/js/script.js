5fad1c529db989212437bcef25435dca0e883c5a
nohadrweesh <noha_drweesh@yahoo.com>
2017-08-11 00:30:10 +0200

    first version

---
+++ resources/js/script.js
@@ -0,0 +1,112 @@
+$(document).ready( function(){
+    /*for sticky navigation*/
+   $('.js--features-section').waypoint(function(direction){
+       
+       if(direction == "down"){
+           $('nav').addClass('sticky');
+       }else{
+            $('nav').removeClass('sticky');
+       }
+   },{
+       offset:'60px'
+   });
+    /*scroll on buttons*/
+    $('.js--scroll-to-plans').click(function(){
+        $('body, html').animate({scrollTop: $('.js--plans-section').offset().top},1000);
+    });
+    $('.js--scroll-to-start').click(function(){
+        $('body, html').animate({scrollTop: $('.js--features-section').offset().top},1000);
+    });
+    /*smooth scrolling*/
+    $(function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 2000);
+            return false;
+               }
+             }
+           });
+         });
+    /*Animation on scroll*/
+    $('.js--wp-1').waypoint(function(direction){
+        $('.js--wp-1').addClass('animated fadeIn');
+        
+    },{ offset: '50%'});
+    
+     $('.js--wp-2').waypoint(function(direction){
+        $('.js--wp-2').addClass('animated fadeInUp');
+        
+    },{ offset: '50%'});
+    
+     $('.js--wp-3').waypoint(function(direction){
+        $('.js--wp-3').addClass('animated fadeIn');
+        
+    },{ offset: '50%'});
+    
+     $('.js--wp-4').waypoint(function(direction){
+        $('.js--wp-4').addClass('animated pulse');
+        
+    },{ offset: '50%'});
+    /*mobile  nav icon*/
+    $('.js--nav-icon').click(function(){
+       var nav = $('.js--main-nav');
+        var icon=$('.js--nav-icon i');
+         nav.slideToggle(200);
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+            
+            
+        }else{ 
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+            }
+      
+    });
+/*gmaps*/
+    var map=new GMaps({
+      div: '.map',
+      lat: 38.7436266,
+      lng:-9.05,
+        zoom:12
+    });
+    map.addMarker({
+   lat: 38.7436266,
+      lng:-9.1602037,
+       
+  title: 'Lisbon',
+  infoWindow: {
+  content: '<p>Our Lisbon HQ</p>'
+}
+});
+    
+});
+
+/*var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})*/
+/*$(function() {
+          $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+        || location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+           if (target.length) {
+             $('html,body').animate({
+                 scrollTop: target.offset().top
+            }, 1000);
+            return false;
+               }
+             }
+           });
+         });*/
